adcf49d01ee8513d635767b99a8ffdf175e9715a
Trevor Jones <degengenesis@protonmail.com>
2022-10-09 13:08:59 -0700

    Add: implement state management

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import { Form, Button } from 'react-bootstrap';
 
 import '../App.css';
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [login, setLogin] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('Submitted');
+  };
+
   return (
     <>
       <h2 className='name'>Sign In!</h2>
       {/* Email */}
-      <Form className='p-3 mt-3'>
+      <Form 
+        className='p-3 mt-3'
+        onSubmit={handleSubmit}
+      >
         <div className='form-field d-flex align-items-center'>
-          <span className='far fa-user'></span>
-          <input type='text' name='Email' placeholder='Enter email' id='email' />
+          <input 
+            type='text' 
+            name='Email' 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}          
+            placeholder='Enter email'
+          />
         </div>
         
         {/* Password */}
         <div className='form-field d-flex align-items-center'>
-          <span className='far fa-user'></span>
-          <input type='text' name='Email' placeholder='Enter password' id='email' />
+          <input 
+            type='password' 
+            name='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} 
+            placeholder='Enter password'
+          />
         </div>
 
         {/* Submit button */}
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,35 +1,66 @@
-import React from 'react';
+import React, { useState} from 'react';
 import ReactDOM from 'react-dom';
 import { Form, Button, Nav } from 'react-bootstrap';
 
 import '../App.css';
 
 const Register = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [register, setRegister] = useState(false);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  alert('Submitted');
+};
+
   return (
     <>
       <div>
         <h2 className='name'>Sign Up!</h2>
         {/* Email */}
-        <Form className='p-3 mt-3'>
+        <Form 
+          className='p-3 mt-3'
+          onSubmit={handleSubmit}
+        >
           <div className='form-field d-flex align-items-center'>
-            <span className='far fa-user'></span>
-            <input type='text' name='Email' placeholder='Enter email' id='email' />
+            <input 
+              className='far fa-user' 
+              type='email'  
+              name='Email' 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Enter email' 
+            />
           </div>
           
           {/* Password */}
-          <div className='form-field d-flex align-items-center'>
-            <span className='far fa-user'></span>
-            <input type='text' name='Email' placeholder='Enter password' id='email' />
+          <div className='form-field d-flex align-items-center'>          
+            <input 
+              className='far fa-user'              
+              type='password' 
+              name='Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} 
+              placeholder='Enter password' 
+            />
           </div>
 
           {/* Submit button */}
-          <Button className='btn mt-3' variant='primary' type='submit'>
+          <Button 
+            className='btn mt-3' 
+            variant='primary'
+            type='submit'
+            onClick={handleSubmit}
+          >
             Submit
           </Button>
         </Form>
       </div> 
       <div class="text-center fs-6">
-            <Nav.Link variant='link'>Forgot password?</Nav.Link> or <Nav.Link variant='link'>Sign In</Nav.Link>
+            <Nav.Link variant='link'>Forgot password?</Nav.Link> or 
+            <Nav.Link variant='link'>Sign In</Nav.Link>
         </div>
     </>
   )

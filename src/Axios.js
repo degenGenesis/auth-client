0f8c5cacf7447c7b04f6c8726ebe52da9629dd7f
Trevor Jones <degengenesis@protonmail.com>
2022-10-27 23:07:13 -0700

    Edit: revert to default export

--- src/Axios.js
+++ src/Axios.js
@@ -1,16 +1,16 @@
 /*{ ---> Axios.js <--- }*/
-import React, {useState} from 'react'
+import React from 'react';
 import axios from "axios";
 import { Formik } from "formik";
 
 const AxiosAPI = (email, password) => {
 
-  const [register, setRegister] = useState(false);
+  // const [register, setRegister] = useState(false);
 
   const config = {
     method: 'post',
     url: 'https://user-auth-v1.herokuapp.com/register',
-    body: JSON.stringify({id: '200'}),
+    // body: JSON.stringify({id: '200'}),
     data: {
       email,
       password,
@@ -19,16 +19,17 @@ const AxiosAPI = (email, password) => {
 
   axios(config)
     .then((response) => {
-      if (response.ok) {
+      /* if (response.ok) {
         return response.json();
-      } 
-      throw new Error('Registration failed!');
-    }, networkError => console.log(networkError.message)
-    )
-    .then(jsonResponse => {
-      return jsonResponse;
+      } */ 
+      console.log(response);
+      // throw new Error('Registration failed!');
+    })
+    .catch((error) => {
+      console.log(error);
     });
-
+    /* .then((jsonResponse) => {
+      return jsonResponse; */
   };
 
-export default AxiosAPI;
+  export default AxiosAPI;

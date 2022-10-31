ce9dad80060c1ba8c0016d83ada770db0c957d7f
Trevor Jones <degengenesis@protonmail.com>
2022-10-31 14:50:53 -0700

    Add: axios, handleSubmit functionality & onSubmit properties

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,41 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import { Form, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Formik, Form, useField, ErrorMessage, Field } from 'formik';
+import * as Yup from 'yup';
+import { Nav } from 'react-bootstrap';
+import axios from 'axios';
 
 import '../App.css';
 
+
 const Login = () => {
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [login, setLogin] = useState(false);
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [login, setLogin] = React.useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert('Submitted');
+    // alert("Submitted");
+    const config = {
+      method: 'post',
+      url: 'https://user-auth-v1.herokuapp.com/login',
+      data: {
+        email,
+        password,
+      },
+    };
+
+    axios(config)
+      .then((response) => {
+        console.log(response);
+        setLogin(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        error = new Error('Login failed!');
+      });
   };
+  
 
   return (
     <>
@@ -21,15 +43,15 @@ const Login = () => {
       {/* Email */}
       <Form 
         className='p-3 mt-3'
-        onSubmit={handleSubmit}
+        onSubmit={(e) => {handleSubmit(e)}}
       >
         <div className='form-field d-flex align-items-center'>
           <input 
-            type='text' 
-            name='Email' 
+            type='email' 
+            name='email' 
             value={email}
-            onChange={(e) => setEmail(e.target.value)}          
             placeholder='Enter email'
+            onChange={(e) => setEmail(e.target.value)}          
           />
         </div>
         
@@ -37,17 +59,31 @@ const Login = () => {
         <div className='form-field d-flex align-items-center'>
           <input 
             type='password' 
-            name='Password'
+            name='password'
             value={password}
-            onChange={(e) => setPassword(e.target.value)} 
             placeholder='Enter password'
+            onChange={(e) => setPassword(e.target.value)} 
           />
         </div>
 
         {/* Submit button */}
-        <Button className='btn mt-3' variant='primary' type='submit'>
+        <button 
+          className='btn mt-3'
+          variant='primary'
+          type='submit'
+          onClick={(e) => handleSubmit(e)}
+        >
           Submit
-        </Button>
+        </button>
+        <div className="text-center fs-6">
+          <Nav.Link variant='link'>
+            Forgot password?
+          </Nav.Link> 
+              or 
+          <Nav.Link href='/Login' variant='link'>
+            Sign Up
+          </Nav.Link>
+        </div>
       </Form>
     </>
   );

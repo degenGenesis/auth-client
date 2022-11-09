60c72cb5b285637f9c1a7f0a569d29078d647fe3
Trevor Jones <degengenesis@protonmail.com>
2022-11-08 23:51:33 -0800

    Edit: wrap <App /> in <BrowserRouter>, refactor for react 18 rendering

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-ReactDOM.render(
+// const rootElement = document.getElementById('root');
+
+// const root = ReactDOMClient.createRoot(rootElement);
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />  
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

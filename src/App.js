d000ca22e9439a30d135c1aa163604a2785e8ad5
Trevor Jones <degengenesis@protonmail.com>
2022-11-08 23:49:16 -0800

    Refactor: return statement and routing

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
 import React from "react";
-import { Routes, Route, } from "react-router-dom";
-import { Container, Col, Row } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+// import { Container, Col, Row } from "react-bootstrap";
 
-import Register from "././components/Register";
-import Register1 from "././components/Register1";
-import Login from "./components/Login";
+import Landing from "./pages/Landing.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
 
 import "./App.css";
 
-const App = () =>{
-  return (
+const App = () => {
+  // return (
 
-    <div className="form-container">
+  //   <div className="form-container">      
+  //       <div>
+  //         <Container fluid='true' className="wrapper">
+  //           <Row>
+  //             <Col xs={12} sm={12} md={12} lg={12}>
+  //               <Register />
+  //             </Col>
+  //             {/* <Col xs={12} sm={12} md={12} lg={6}>
+  //               <Login />
+  //             </Col> */}
+  //           </Row>
+            
+            
+            
+  //         </Container>
+  //         {/* Routes */}
+  //       <Routes>
+  //         <Route>
+  //           {/* Forms */}
+  //           {/* <Route exact path='/register' element={(<Register />)} /> */}
+  //           {/* <Route path='/login' element={(<Login />)} /> */}
+  //           {/* <Route path='/resetPassword' element={ResetPassword} /> */}
+  //         </Route>
+  //       </Routes>
+  //       </div>  
+      
+  //   </div>   
+  // );
+
+  return (
       <div>
-        <Container fluid='true' className="wrapper">
-          <Row>
-            <Col xs={12} sm={12} md={12} lg={12}>
-              <Register1 />
-            </Col>
-            {/* <Col xs={12} sm={12} md={12} lg={6}>
-              <Login />
-            </Col> */}
-          </Row>
-          
-          
-          
-        </Container>
-        {/* Routes */}
+        <div className='landing'>
         <Routes>
-          {/* Forms */}
-          {/* <Route path='/' element={(<Register />)} /> */}
-          {/* <Route path='/login' element={(<Login />)} /> */}
-          {/* <Route path='/resetPassword' element={ResetPassword} /> */}
+          <Route path='/' element={<Landing />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
-      </div>  
-    </div>   
-  );
+        </div>
+      </div>
+  )
 }
 
 export default App;

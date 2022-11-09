723819f169c981bbc5e76d6535659bcda01d97ae
Trevor Jones <degengenesis@protonmail.com>
2022-11-09 01:00:59 -0800

    Edit: refactor Login page && Add: LoginForm component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,92 +6,14 @@ import { Nav, Form, Button } from 'react-bootstrap';
 import axios from 'axios';
 
 import '../App.css';
+import LoginForm from '../components/forms/LoginForm';
 
-
-export default function Login () {
-
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [login, setLogin] = React.useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // alert("Submitted");
-    const config = {
-      method: 'post',
-      url: 'https://user-auth-v1.herokuapp.com/login',
-      data: {
-        email,
-        password,
-      },
-    };
-
-    axios(config)
-      .then((response) => {
-        console.log(response);
-        setLogin(true);
-      })
-      .catch((error) => {
-        console.log(error);
-        error = new Error('Login failed!');
-      });
-  };
-  
-
+const Login = () => {
   return (
     <>
-      <h2 className='name'>Sign In!</h2>
-      {/* Email */}
-      <Form 
-        className='p-3 mt-3'
-        onSubmit={(e) => {handleSubmit(e)}}
-      >
-        <div className='form-field d-flex align-items-center'>
-          <input 
-            type='email' 
-            name='email' 
-            value={email}
-            placeholder='Enter email'
-            onChange={(e) => setEmail(e.target.value)}          
-          />
-        </div>
-        
-        {/* Password */}
-        <div className='form-field d-flex align-items-center'>
-          <input 
-            type='password' 
-            name='password'
-            value={password}
-            placeholder='Enter password'
-            onChange={(e) => setPassword(e.target.value)} 
-          />
-        </div>
-
-        {/* Submit button */}
-        <Button 
-          className='btn mt-3'
-          variant='primary'
-          type='submit'
-          onClick={(e) => handleSubmit(e)}
-        >
-          Submit
-        </Button>
-        <div className="links">
-          <div className="text-center fs-6">
-            <Link variant='link'>
-              Forgot password?
-            </Link>
-          </div>
-          <div className="text-center fs-6">
-                or
-          </div> 
-          <div className="text-center fs-6">
-            <Link to='/Login' variant='link'>
-              Sign Up
-            </Link>
-          </div>
-        </div>
-      </Form>
+      <LoginForm />
     </>
-  );
+  )
 };
+
+export default Login;

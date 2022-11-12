96252a817eeb96d6cab6d8efc99dbf90c3d0a4ce
Trevor Jones <degengenesis@protonmail.com>
2022-11-12 00:02:27 -0800

    Add: reset password form route

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Landing from './pages/Landing'
+
+const App = () => {
+  return (
+    <div>
+      <div className='landing'>
+        <Landing />
+      </div>
+    </div>
+  )
+}
+
+export default App

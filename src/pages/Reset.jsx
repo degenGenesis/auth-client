bd717614a2d8e6e802fd0a8a202fec8153acfa0d
Trevor Jones <degengenesis@protonmail.com>
2022-11-12 00:01:32 -0800

    Add: import & implement ResetForm component

---
+++ src/pages/Reset.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ResetForm from '../components/forms/ResetForm';
+
+const Reset = () => {
+  return (
+    <div>
+      <ResetForm />
+    </div>
+  )
+};
+
+export default Reset;

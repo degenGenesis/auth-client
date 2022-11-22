d8c2e0fb38a55a9a74c8fa60f6e6ad1c44f8a9f2
Trevor Jones <degengenesis@protonmail.com>
2022-11-21 21:45:31 -0800

    Edit: page structure

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Login from "./pages/Login.jsx";
 import Reset from "./pages/Reset.jsx";
 
 import "./App.css";
+import Header from "./components/Header.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
   // return (
@@ -42,16 +44,21 @@ const App = () => {
   // );
 
   return (
-      <div>
-        <div className='landing'>
+    <div>
+      <div className="landing">
+        <Header />
+        <Navbar />
+      </div>  
+      
+      <div className='body'>
         <Routes>
           <Route path='/' element={<Landing />} />
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
           <Route path='/reset' element={<Reset />} />
         </Routes>
-        </div>
       </div>
+    </div>
   )
 }
 
